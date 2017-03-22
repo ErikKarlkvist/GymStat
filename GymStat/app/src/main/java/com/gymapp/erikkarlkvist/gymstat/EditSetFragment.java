86e315687b183164d3f9b1dccd2e8842e8536ed9
Filip Ödeen <filipodeen@Filips-MacBook-Pro.local>
2017-03-22 16:55:11 +0100

    added edit set interface

---
+++ GymStat/app/src/main/java/com/gymapp/erikkarlkvist/gymstat/EditSetFragment.java
@@ -0,0 +1,25 @@
+package com.gymapp.erikkarlkvist.gymstat;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class EditSetFragment extends Fragment {
+
+    public EditSetFragment(){}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.edit_set, container, false);
+    }
+
+
+}
